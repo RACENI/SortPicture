aeb8f14fc5f10275f9ccac97193b380c20fba63b
RACENI <saracen_dev@naver.com>
2024-08-13 00:39:19 +0900

    -메타데이터 불러오는 라이브러리 변경(MetadataExtractor) -날짜기반 사진정리 수정 -GPS 기반 사진 정리 기틀 마련

--- picturemetadata/Program.cs
+++ picturemetadata/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Windows.Media.Imaging;
+using MetadataExtractor.Formats.Exif;
+using System.Linq;
+using MetadataExtractor.Formats.Iptc;
+using MetadataExtractor;
+using Directory = System.IO.Directory;
 
 namespace picturemetadata
 {
     class Program
     {
-        static List<String> lst = new List<string>(); // 분류한 파일 경로를 담은 리스트
-        static int count = 0; // 총 변경한 파일 갯수
-        
         static void Main(string[] args)
         {
             Console.WriteLine("사진 정리 프로그램에 오신 것을 환영합니다.");
@@ -21,40 +22,56 @@ namespace picturemetadata
             Console.WriteLine("2. working폴더 안에 정리할 사진파일을 넣는다.");
             Console.WriteLine("3. 준비가 완료 되었으면 프로그램을 재시작합니다.");
             Console.WriteLine("---------------------------------------------.");
-            Console.WriteLine("계속 하실 거면 '1'을 입력하시고 엔터를 눌러주십시오. 그렇지 아니하면 엔터를 눌러주십시오.");
-            Console.WriteLine("단, 기존 파일은 삭제됩니다.");
+            Console.WriteLine("날짜기반정리는 '1'을 입력하십시오.");
+            Console.WriteLine("GPS기반정리는 '2'를 입력하십시오.(미구현)");
+            Console.WriteLine("프로그램 종료는 '-1'를 입력하십시오.");
             try
             {
                 int check = Convert.ToInt32(Console.ReadLine());
 
+                if (check < 0)
+                    return; // 프로그램 종료
+
+                List<string> sortFilePaths = new List<string>(); // 분류된 파일 경로 리스트
+                string Path = Directory.GetCurrentDirectory() + @"\working"; // 기준 경로
+
+                if (Directory.Exists(Path))
+                {
+                    DirectoryInfo dirInfo = new DirectoryInfo(Path);
+
+                    switch (check)
+                    {
+                        case 1:
+                            sortFilePaths = sortPictureDate(dirInfo); // 날짜기반 파일 정리
+                            break;
+                        case 2:
+                            sortFilePaths = sortPictureGPS(dirInfo); // GPS기반 파일 정리
+                            break;
+                        default:
+                            return;
+                    }
+
+                    Console.WriteLine("총 파일 수 : " + dirInfo.GetFiles().Length);
+                    Console.WriteLine("완료한 파일 수 : " + sortFilePaths.Count);
+                }
+                else
+                {
+                    Console.WriteLine("working 폴더가 없습니다.");
+                }
+
+                Console.WriteLine("---------------------------------------------.");
+                Console.WriteLine("기존 파일을 삭제하려면 '1'을 입력해주세요.");
+                Console.WriteLine("프로그램 종료를 원하시면 엔터를 누르십시오.");
+                check = Convert.ToInt32(Console.ReadLine());
+
+                //기존 파일 삭제
                 if (check == 1)
                 {
-                    int total = 0;
-                    string Path = @Directory.GetCurrentDirectory() + @"\working";
-
-                    if (Directory.Exists(Path))
+                    foreach (string sortFilePath in sortFilePaths)
                     {
-                        DirectoryInfo di = new DirectoryInfo(Path);
-
-                        foreach (var item in di.GetFiles())
-                        {
-                            sortPicture(item.Name);
-                            total++;
-                        }
-                        foreach (string a in lst)
-                        {
-                            File.Delete(a);
-                        }
-
-                        Console.WriteLine("총 파일 수 : " + total);
-                        Console.WriteLine("완료한 파일 수 : " + count);
-
+                        File.Delete(sortFilePath);
                     }
-                    else
-                    {
-                        Console.WriteLine("working 폴더가 없습니다.");
-                    }
-                    Console.WriteLine("엔터를 눌러주십시오.");
+                    Console.WriteLine("삭제완료!");
                     Console.ReadLine();
                 }
             }
@@ -62,43 +79,74 @@ namespace picturemetadata
             {
 
             }
-            
         }
 
-        
-        //사진 정렬
-        static public void sortPicture(string fileName)
+
+        /// <summary>
+        /// 날짜기반 사진 정리 메소드
+        /// </summary>
+        /// <param name="dirInfo"></param>
+        /// <returns>분류된 파일들의 경로 리스트</returns>
+        static private List<string> sortPictureDate(DirectoryInfo dirInfo)
         {
-            string path = @"working/" + fileName;
-            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            List<string> doneFilePath = new List<string>(); // 완료한 파일 경로 저장 리스트
+            foreach (var file in dirInfo.GetFiles())
             {
+                string path = @"working/" + file.Name;
+
                 try
                 {
-                    BitmapSource img = BitmapFrame.Create(fs);
-                    BitmapMetadata md = (BitmapMetadata)img.Metadata;
-
-                    string date = md.DateTaken; // 사진 메타데이터의 date 가져오기
+                    var directories = ImageMetadataReader.ReadMetadata(path);
+                    var exifDirectory = directories.OfType<ExifSubIfdDirectory>().FirstOrDefault(); // EXIF에서 날짜를 가져옴.
+                    string dateTimeOriginal = exifDirectory.GetDateTime(ExifDirectoryBase.TagDateTimeOriginal).ToString(); // 년월일만 사용할 예정이므로 형변환
 
                     char sp = ' ';
+                    string[] spstring = dateTimeOriginal.Split(sp); // ["yyyy-mm-dd", "오전/오후", "hh:mm:ss"] 배열 생성
 
-                    string[] spstring = date.Split(sp);
+                    Console.WriteLine("완료한 파일 : " + path + " , 찍은 날짜 : " + spstring[0]);
 
-                    Console.WriteLine("완료한 파일 : "+ path + " , 찍은 날짜 : "+spstring[0]);
+                    string thisDir = Directory.GetCurrentDirectory(); // 현재 디렉터리 주소
+                    if (Directory.Exists(thisDir + "\\" + spstring[0]) == false)
+                        Directory.CreateDirectory(thisDir + "\\" + spstring[0]); // 날짜에 맞는 폴더가 없을 시 폴더 생성
 
-
-                    if(Directory.Exists(@Directory.GetCurrentDirectory() + "\\" + spstring[0]) == false)
-                    {
-                        Directory.CreateDirectory(@Directory.GetCurrentDirectory() + "\\" + spstring[0]);
-                    }
-                    File.Copy(Directory.GetCurrentDirectory() + "\\" + path, Directory.GetCurrentDirectory() + "\\" + spstring[0] + "\\" + fileName, true);
-                    lst.Add(Directory.GetCurrentDirectory() + "\\" + path); // 기존파일 삭제를 위한 리스트 담기
-                    count++;
+                    File.Copy(thisDir + "\\" + path, thisDir + "\\" + spstring[0] + "\\" + file.Name, true); // 메타데이터에 해당하는 날짜에 추가
+                    doneFilePath.Add(path);
                 }
-                catch
+                catch(NullReferenceException e)
                 {
-                    Console.WriteLine("본 파일은 사진파일이 아닙니다.("+ path + ")");
+                    Console.WriteLine("본 파일은 사진파일이 아니거나 날짜 메타데이터가 존재하지 않습니다.(" + path + ")");
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("알 수 없는 오류로 처리하지 못했습니다.(" + path + ")");
                 }
             }
+            return doneFilePath;
+        }
+
+        /// <summary>
+        /// GPS기반 사진 정리 메소드
+        /// </summary>
+        /// <param name="dirInfo"></param>
+        /// <returns>분류된 파일들의 경로 리스트</returns>
+        static private List<string> sortPictureGPS(DirectoryInfo dirInfo)
+        {
+            string path = @"working/" + dirInfo;
+
+            var directories = ImageMetadataReader.ReadMetadata(path);
+
+            var gpsDirectory = directories.OfType<GpsDirectory>().FirstOrDefault();
+
+            if (gpsDirectory != null)
+            {
+                var location = gpsDirectory.GetGeoLocation();
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}");
+                }
+            }
+            return null;
         }
     }
 }
+
